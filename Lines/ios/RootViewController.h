c0643d796147abad658dba0ca9196c97e87fa6ee
CpyShine <backjy@foxmail.com>
2013-04-26 09:58:11 +0800

    Initial Commit

---
+++ Lines/ios/RootViewController.h
@@ -0,0 +1,16 @@
+//
+//  LinesAppController.h
+//  Lines
+//
+//  Created by CpyShine on 13-4-26.
+//  Copyright __MyCompanyName__ 2013年. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+
+@interface RootViewController : UIViewController {
+
+}
+
+@end
---
+++ Lines/ios/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  Lines
+//
+//  Created by CpyShine on 13-4-26.
+//  Copyright __MyCompanyName__ 2013年. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+int main(int argc, char *argv[]) {
+    
+    NSAutoreleasePool * pool = [[NSAutoreleasePool alloc] init];
+    int retVal = UIApplicationMain(argc, argv, nil, @"AppController");
+    [pool release];
+    return retVal;
+}
